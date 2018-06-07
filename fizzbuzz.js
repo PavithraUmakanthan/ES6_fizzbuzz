4569b8ccc6b4aed18aa89e2a55ee5803c34d72d0
Pavithra Umakanthan <pavithraumakanthan@Pavithras-MacBook-Pro.local>
2018-06-07 13:44:22 -0700

    Updated the main function to print the return value from the fizzbuzz function.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,18 @@
 
 const fizzbuzz = (x) => {
         if ((x % 3 === 0)&& (x % 5 === 0)){
-            console.log(`fizzbuzz`);
+            return `fizzbuzz`;
         } else if (x % 3 === 0) {
-            console.log(`fizz`);
+            return `fizz`;
         } else if(x % 5 === 0){
-            console.log(`buzz`);
-        } else{
-          	console.log(x);
+            return `buzz`;
+        } else {
+          	return x;
         }
     };
 const main = () => {
     for (var i = 1; i <= 100; i++) {
-		fizzbuzz(i);
+		console.log(fizzbuzz(i));
      }
   }
 main();

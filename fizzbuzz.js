b9cbc370456215d4855bae94ee86d86a4b2b9672
Pavithra Umakanthan <pavithraumakanthan@Pavithras-MacBook-Pro.local>
2018-06-12 16:56:32 -0700

    Reverting back the for..of loop changes to for loop

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,16 @@
-const fizzbuzz = number => {
-  if (number % 3 === 0 && number % 5 === 0) {
-    return 'fizzbuzz';
+const fizzbuzz = (number) => {
+  if ((number % 3 === 0) && (number % 5 === 0)) {
+      return 'fizzbuzz';
   } else if (number % 3 === 0) {
-    return 'fizz';
+      return 'fizz';
   } else if (number % 5 === 0) {
-    return 'buzz';
+      return 'buzz';
   }
   return number;
 }
-
+    
 const main = () => {
-  const array = [...Array(100).keys()].map(x => ++x);
-  for (const i of array) {
+  for (var i = 1; i <= 100; i++) {
     console.log(fizzbuzz(i));
   }
 }
@@ -19,8 +18,9 @@ main();
 
 const test = () => {
   const array = [4, 15, 20, 21];
-  for (const i of array) {
-    fizzbuzz(array[i]);
+  for(const i of array) {  
+    fizzbuzz(array[i]); 
   }
 }
 test();
+

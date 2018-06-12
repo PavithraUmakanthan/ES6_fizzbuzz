ffc1b90ecc45ef7cec903116d9447926fa31dcff
Pavithra Umakanthan <pavithraumakanthan@Pavithras-MacBook-Pro.local>
2018-06-12 14:40:12 -0700

    Corrected the identation.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,26 +1,26 @@
-const fizzbuzz = (x) => {
- if ((x % 3 === 0)&&(x % 5 === 0)) {
-  return 'fizzbuzz';
- } else if (x % 3 === 0) {
-  return 'fizz';
- } else if (x % 5 === 0) {
-  return 'buzz';
- } else {
-  return x;
- }
-};
-    
-const main = () => {
- for (var i = 1; i <= 100; i++) {
-  console.log(fizzbuzz(i));
- }
-}
-main();
+  const fizzbuzz = (number) => {
+    if ((number % 3 === 0) && (number % 5 === 0)) {
+        return 'fizzbuzz';
+    } else if (number % 3 === 0) {
+        return 'fizz';
+    } else if (number % 5 === 0) {
+        return 'buzz';
+    } else {
+        return number;
+    }
+  };
+      
+  const main = () => {
+    for (var i = 1; i <= 100; i++) {
+      console.log(fizzbuzz(i));
+    }
+  }
+  main();
 
-const test = () => {
- var array=[4,15,20,21];
- for(var i = 0;i<=3;i++) {            
-  fizzbuzz(array[i]); 
- }
-}
-test();
+  const test = () => {
+    const array = [4, 15, 20, 21];
+    for(var i = 0; i <= 3; i++) {  
+      fizzbuzz(array[i]); 
+    }
+  }
+  test();

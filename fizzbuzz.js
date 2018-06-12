016b644d6178d7db139a4109e43e53e8f055ce17
Pavithra Umakanthan <pavithraumakanthan@Pavithras-MacBook-Pro.local>
2018-06-12 15:45:11 -0700

    Updated the loop with for..of and removed the else statement in the fizzbuzz function.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,26 +1,26 @@
-  const fizzbuzz = (number) => {
-    if ((number % 3 === 0) && (number % 5 === 0)) {
-        return 'fizzbuzz';
-    } else if (number % 3 === 0) {
-        return 'fizz';
-    } else if (number % 5 === 0) {
-        return 'buzz';
-    } else {
-        return number;
-    }
-  };
-      
-  const main = () => {
-    for (var i = 1; i <= 100; i++) {
-      console.log(fizzbuzz(i));
-    }
+const fizzbuzz = (number) => {
+  if ((number % 3 === 0) && (number % 5 === 0)) {
+      return 'fizzbuzz';
+  } else if (number % 3 === 0) {
+      return 'fizz';
+  } else if (number % 5 === 0) {
+      return 'buzz';
   }
-  main();
+      return number;
+};
+    
+const main = () => {
+  for (var i = 1; i <= 100; i++) {
+    console.log(fizzbuzz(i));
+  }
+}
+main();
 
-  const test = () => {
-    const array = [4, 15, 20, 21];
-    for(var i = 0; i <= 3; i++) {  
-      fizzbuzz(array[i]); 
-    }
+const test = () => {
+  const array = [4, 15, 20, 21];
+  for(const i of array) {  
+    fizzbuzz(array[i]); 
   }
-  test();
+}
+test();
+

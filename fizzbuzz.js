55c2305900e82b16d4b38622741cc60e7e98f633
Pavithra Umakanthan <pavithraumakanthan@Pavithras-MacBook-Pro.local>
2018-06-12 14:10:35 -0700

    Changed the indentation and return statement as per Staphanie's feedback

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,26 +1,26 @@
 const fizzbuzz = (x) => {
-        if ((x % 3 === 0)&& (x % 5 === 0)) {
-            return `fizzbuzz`;
-        } else if (x % 3 === 0) {
-            return `fizz`;
-        } else if (x % 5 === 0) {
-            return `buzz`;
-        } else {
-          	return x;
-        }
-    };
+ if ((x % 3 === 0)&&(x % 5 === 0)) {
+  return 'fizzbuzz';
+ } else if (x % 3 === 0) {
+  return 'fizz';
+ } else if (x % 5 === 0) {
+  return 'buzz';
+ } else {
+  return x;
+ }
+};
     
 const main = () => {
-   for (var i = 1; i <= 100; i++) {
-		console.log(fizzbuzz(i));
-     }
-  }
+ for (var i = 1; i <= 100; i++) {
+  console.log(fizzbuzz(i));
+ }
+}
 main();
 
 const test = () => {
-    var array=[4,15,20,21];
-    for(var i = 0;i<=3;i++) {            
-        fizzbuzz(array[i]);
-}
+ var array=[4,15,20,21];
+ for(var i = 0;i<=3;i++) {            
+  fizzbuzz(array[i]); 
+ }
 }
 test();
